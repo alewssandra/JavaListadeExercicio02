f7f865526ec357965c83b1086b48c8a009220b85
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-06-26 20:16:32 -0300

    Add files via upload

---
+++ exercicio1/Pessoa.java
@@ -0,0 +1,38 @@
+package n1;
+
+public class Pessoa {
+    
+    //Atributos
+    protected String nome;
+    protected int cpf;
+    
+    //Construtor
+    public Pessoa(String nome, int cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+    //Métodos
+    public void fala(){}
+    
+    public void perfil(){}
+    
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    
+}
