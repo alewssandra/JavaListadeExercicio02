64677006414c1922d4db8e3cbb53ba416eeaa292
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-06-26 20:10:54 -0300

    Add files via upload

---
+++ Exercicio2/Gato.java
@@ -0,0 +1,16 @@
+package n2;
+
+public class Gato extends Animal {
+
+    //Construtor
+    public Gato(String nome, float peso) {
+        super(nome, peso);
+    }
+
+    //Métodos
+    @Override
+    public void emitirSom() {
+        System.out.println("Miaaaaaaaaaaau");
+    }
+
+}
